0c1187ee45deb8f68cac11af22f07519c3bc7e1b
Elerinna2a <114982334+Elerinna2a@users.noreply.github.com>
2023-03-12 08:27:01 +0100

    a

--- apps/client/src/components/CreateCollecte.tsx
+++ apps/client/src/components/CreateCollecte.tsx
@@ -1,19 +1,9 @@
 import { Button, Heading, Input, VStack } from "@chakra-ui/react";
-import { useMutation, useQueryClient } from "react-query";
+import { useQueryClient } from "react-query";
 import { useNavigate } from "react-router-dom";
-import { createCollecte } from "../utils/collecte.fetcher";
 
 export default function CreateCollecte() {
   const navigate = useNavigate();
-  const createCollecteMutation = useMutation(createCollecte, {
-    onSuccess: ({ idNumLot }) => {
-      queryClient.refetchQueries(["collectes"]);
-      if (idNumLot?.current?.value) idNumLot.current.value = "";
-    },
-    onError: () => {
-      console.log("Error fetching queries");
-    },
-  });
 
   const queryClient = useQueryClient();
 
